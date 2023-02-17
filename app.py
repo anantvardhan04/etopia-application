c8925bfad5d58bfb4375e17809d4c3e212c1a420
Anant Vardhan <anant_vardhan@vfc.com>
2023-02-18 01:35:51 +0530

    Added application logic

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os, logging, requests, re
+import mysql.connector
 from slack_sdk.errors import SlackApiError
 from slack_bolt import App
+
+
 
 logging.basicConfig(level=logging.INFO)
 app = App(
@@ -8,14 +11,14 @@ app = App(
     signing_secret="SLACK_SIGNING_SECRET"
 )
 
-channel_id = "C04KMTGGV8X"
+channel_id = "C04QFLU974Z"
 
 ##########################################
 ###########CREATE ORDER CMD###############
 ##########################################
 
 # Listen to /createorder command events	
-@app.command("/createorder")
+@app.command("/post")
 def handle_command(body, ack, client, logger):
     logger.info(body)
     ack()
@@ -25,93 +28,16 @@ def handle_command(body, ack, client, logger):
     "close": {"type": "plain_text", "text": "Cancel"},
 	"title": {
 		"type": "plain_text",
-		"text": "Bulk Order Creation App"
+		"text": "Fonetwish Application"
 	},
 	"submit": {
 		"type": "plain_text",
-		"text": "Place Order"
+		"text": "Post Message"
 	},
-    "callback_id": "create-order-model",
-    "close": {"type": "plain_text", "text": "Cancel"},
+    "callback_id": "post-message",
 	"blocks": [
-		{
-			"type": "input",
-            "block_id": "brand",
-			"element": {
-				"type": "static_select",
-				"placeholder": {
-					"type": "plain_text",
-					"text": "Select a Brand"
-				},
-				"options": [
-					{
-						"text": {
-							"type": "plain_text",
-							"text": "eastpak"
-						},
-						"value": "eastpak"
-					},
-					{
-						"text": {
-							"type": "plain_text",
-							"text": "kipling"
-						},
-						"value": "kipling"
-					},
-					{
-						"text": {
-							"type": "plain_text",
-							"text": "dickieslife"
-						},
-						"value": "dickieslife"
-					}
-				],
-				"action_id": "static_select-action"
-			},
-			"label": {
-				"type": "plain_text",
-				"text": "Brand"
-			}
-		},{
-			"type": "input",
-            "block_id": "environment",
-			"element": {
-				"type": "static_select",
-				"placeholder": {
-					"type": "plain_text",
-					"text": "Select an Environment"
-				},
-				"options": [
-					{
-						"text": {
-							"type": "plain_text",
-							"text": "dev"
-						},
-						"value": "dev"
-					},
-					{
-						"text": {
-							"type": "plain_text",
-							"text": "qa"
-						},
-						"value": "qa"
-					},
-					{
-						"text": {
-							"type": "plain_text",
-							"text": "uat"
-						},
-						"value": "uat"
-					}
-				],
-				"action_id": "static_select-action"
-			},
-			"label": {
-				"type": "plain_text",
-				"text": "Environment"
-			}
-		},
-        {   "block_id": "no_of_orders",
+        {   
+			"block_id": "user",
 			"type": "input",
 			"element": {
 				"type": "plain_text_input",
@@ -119,10 +45,10 @@ def handle_command(body, ack, client, logger):
 			},
 			"label": {
 				"type": "plain_text",
-				"text": "Number of Orders"
+				"text": "User"
 			}
 		},
-        {   "block_id": "product_id",
+        {   "block_id": "message",
 			"type": "input",
 			"element": {
 				"type": "plain_text_input",
@@ -130,70 +56,117 @@ def handle_command(body, ack, client, logger):
 			},
 			"label": {
 				"type": "plain_text",
-				"text": "Product IDs"
-			}
-		},		
-		{
-			"type": "input",
-            "block_id": "payment_method",
-			"element": {
-				"type": "static_select",
-				"placeholder": {
-					"type": "plain_text",
-					"text": "Select a Payment Method"
-				},
-				"options": [
-					{
-						"text": {
-							"type": "plain_text",
-							"text": "Credit Card"
-						},
-						"value": "CreditCard"
-					},
-					{
-						"text": {
-							"type": "plain_text",
-							"text": "checkmo"
-						},
-						"value": "checkmo"
-					}
-				],
-				"action_id": "static_select-action"
-			},
-			"label": {
-				"type": "plain_text",
-				"text": "Payment Method"
+				"text": "Message"
 			}
 		}
 	],
 	"type": "modal"
-},
+     },
     )
-    logger.info(res)	
-# Listens to events from create-order-model callback-id
-@app.view("create-order-model")
+
+# Listen to /createorder command events	
+@app.command("/get")
+def handle_command(body, ack, client, logger):
+    logger.info(body)
+    ack()
+    res = client.views_open( 	
+        trigger_id=body["trigger_id"],
+        view={
+    "close": {"type": "plain_text", "text": "Cancel"},
+	"title": {
+		"type": "plain_text",
+		"text": "Fonetwish Application"
+	},
+	"submit": {
+		"type": "plain_text",
+		"text": "Get Updates"
+	},
+    "callback_id": "get-message",
+	"blocks": [
+        {   
+			"block_id": "user",
+			"type": "input",
+			"element": {
+				"type": "plain_text_input",
+				"action_id": "plain_text_input-action"
+			},
+			"label": {
+				"type": "plain_text",
+				"text": "User"
+			}
+		}
+	],
+	"type": "modal"
+     },
+    )
+
+# Listens to events from post-message callback-id
+@app.view("post-message")
 def view_submission(ack, body, client, logger):
     ack()
+    user_name = body["view"]["state"]["values"]["user"]["plain_text_input-action"]["value"]
+    message =  body["view"]["state"]["values"]["message"]["plain_text_input-action"]["value"]
 
-    brand = body["view"]["state"]["values"]["brand"]["static_select-action"]["selected_option"]["value"]
-    environment = body["view"]["state"]["values"]["environment"]["static_select-action"]["selected_option"]["value"]
-    no_of_orders = body["view"]["state"]["values"]["no_of_orders"]["plain_text_input-action"]["value"]
-    product_id = body["view"]["state"]["values"]["product_id"]["plain_text_input-action"]["value"]
-    payment_method = body["view"]["state"]["values"]["payment_method"]["static_select-action"]["selected_option"]["value"]
-    user = body["user"]["id"] 
-
-    jenkins_job_url = f"https://jenkins_bot:JENKINS_BOT_AUTH@cd.emea.vfc-int.com/job/bulk-order-creation-dev-job/buildWithParameters?token=mytoken123&BRAND={brand}&ENVIRONMENT={environment}&NO_OF_ORDERS={no_of_orders}&PRODUCT_ID={product_id}&PAYMENT_METHOD={payment_method}&SLACK_USER_ID={user}"
-    
-    r = requests.get(url = jenkins_job_url)
-    try:
-	    # Call the conversations.list method using the WebClient	
+   	try:
+		conn = mysql.connector.connect(
+        host="etopia-dev-cluster.cluster-c0zdw8u2xc3b.us-east-1.rds.amazonaws.com",
+        user="admin",
+        password="ETOPIA_DB_PASSWORD",
+        database="etopia_master"
+        )
+	    # Create a cursor
+        cur = conn.cursor()
+        cur.execute("""
+            SET @user_name = %s;
+            SET @message = %s;
+            SELECT COUNT(*) INTO @user_count FROM user_table WHERE user_name = @user_name;
+            IF @user_count = 0 THEN
+                INSERT INTO user_table (user_name) VALUES (@user_name);
+            END IF;
+            INSERT INTO message_table (user_name, message) VALUES (@user_name, @message);
+         """, (user_name, message)
+	    )
 	    result = client.chat_postMessage(
 	        channel=channel_id,
-	        text=f"Hi <@{user}>! Your Order has been placed. You'll soon be notified about your order ids !"
+	        text=f"Hi {user_name}! Your message has been posted."
 	    )
-	    print(result)
     except SlackApiError as e:
 	    print(f"Error: {e}")
+
+# Listens to events from post-message callback-id
+@app.view("get-message")
+def view_submission(ack, body, client, logger):
+    ack()
+    user_name = body["view"]["state"]["values"]["user"]["plain_text_input-action"]["value"]
+   	try:
+		conn = mysql.connector.connect(
+        host="etopia-dev-cluster.cluster-c0zdw8u2xc3b.us-east-1.rds.amazonaws.com",
+        user="admin",
+        password="ETOPIA_DB_PASSWORD",
+        database="etopia_master"
+        )
+	    # Create a cursor
+        cur = conn.cursor()
+        cur.execute("""
+		    SET @user_name = %s
+            SELECT message
+            FROM message_table
+            WHERE user_name = @user_name
+            ORDER BY id DESC
+            LIMIT 3
+        """, (user_name))
+		# Fetch the results and store them in a variable
+        messages = [row[0] for row in cur.fetchall()]
+		cur.close()
+		conn.close()
+
+	    result = client.chat_postMessage(
+	        channel=channel_id,
+	        text='\n'.join([f'Message {i+1}: {message}' for i, message in enumerate(messages)])
+	    )
+    except SlackApiError as e:
+	    print(f"Error: {e}")
+
 
 if __name__ == "__main__":
     app.start(3000)
